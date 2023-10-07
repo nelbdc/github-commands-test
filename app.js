739513f441de1ed952df5971970d9e2356d89b1c
Nelson <neldecas12@gmail.com>
2023-10-07 15:17:19 -0500

    test anything

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 async function fetch() {
     const axios = require('axios');
     const res = await axios("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } });
+    console.log(process.env.ENV_PATH)
     return res.data;
 }
 
