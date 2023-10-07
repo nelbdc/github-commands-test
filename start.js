6d8658470a032fa453fa43a83ef2a65eeb62d306
Nelson <neldecas12@gmail.com>
2023-10-07 15:21:32 -0500

    docker again

--- start.js
+++ start.js
@@ -6,18 +6,24 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+async function start() {
+    const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri);
 
-console.log('Trying to connect to db');
+    console.log('Trying to connect to db');
 
-try {
-    await client.connect();
-    await client.db(dbName).command({ ping: 1 });
-    console.log('Connected successfully to server');
-} catch (error) {
-    console.log('Connection failed.');
-    await client.close();
-    console.log('Connection closed.');
-    process.exit(1);
+    try {
+        await client.connect();
+        await client.db(dbName).command({ ping: 1 });
+        console.log('Connected successfully to server');
+    } catch (error) {
+        console.log('Connection failed.');
+        await client.close();
+        console.log('Connection closed.');
+        process.exit(1);
+    }
+
+    console.log("Job finished")
 }
+
+start();
